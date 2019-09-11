5960997a86613dc93da1a1e2bfff1e148d468821
Marlone Akidiva <marlone911@gmail.com>
2019-09-11 16:12:18 +0300

    Coming Soon Page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import LandingPage from "./components/pages/LandingPage";
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    color: ${props => (props.whiteColor ? "white" : "black")};
+    font-family: ${props => props.theme.fontFamily};
+    font-weight: 300;
+  }
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={{ fontFamily: "Futura" }}>
+      <>
+        <LandingPage />
+        <GlobalStyle whiteColor />
+      </>
+    </ThemeProvider>
   );
-}
-
+};
 export default App;
